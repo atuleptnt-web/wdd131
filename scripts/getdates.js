16faff4c8ace6656c1e14987bb9c07db9e245a1b
paultetteh <pnerquaye@byupathway.edu>
2025-11-01 21:03:30 +0000

    updates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("footerCopyright").innerHTML =
-  `&copy; ${currentYear} Paul Tetteh Nerquaye-Tetteh 🇬🇭 Ghana<br>Note this CodePen ☼ JavaScript Date Object summary of using the Date object in different ways.`;
+  `&copy; ${currentYear} Paul Tetteh Nerquaye-Tetteh Ghana<br>.`;
 
 document.getElementById("lastModified").innerHTML =
   `Final Modification: ${document.lastModified}`;
