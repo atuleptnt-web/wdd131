47f5ded1c8973dd7d8b9eaeacf72ccff178ce2a3
paultetteh <pnerquaye@byupathway.edu>
2025-11-01 19:26:58 +0000

    updated the footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,7 +3,7 @@ const currentYear = new Date().getFullYear();
 
 // Output the copyright year and message in the first paragraph of the footer
 document.getElementById("footerCopyright").innerHTML =
-  `&copy; ${currentYear} — Note this CodePen ☼ JavaScript Date Object summary of using the Date object in different ways.`;
+  `&copy; ${currentYear} — PTNT`;
 
 // Output the last modified date in the second paragraph of the footer
 document.getElementById("lastModified").innerHTML = document.lastModified;
